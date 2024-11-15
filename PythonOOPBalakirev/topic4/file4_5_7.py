0266f66764956a3ad1d4917ee10cd89667c094c5
ilyapopov400 <147233832+ilyapopov400@users.noreply.github.com>
2024-11-15 10:51:13 +0300

    file4_5_7.py

--- PythonOOPBalakirev/topic4/file4_5_7.py
+++ PythonOOPBalakirev/topic4/file4_5_7.py
@@ -1,6 +1,3 @@
-# import pytest
-
-
 class PointTrack:
     def __init__(self, x: (int, float), y: (int, float)):
         for date in (x, y):
@@ -22,8 +19,16 @@ class PointTrack:
 
 
 class Track:
+    """
+    - класс для представления маршрутов в навигаторе
+    """
+
     def __init__(self, *args):
         self.__points = self.__date_of_points(*args)
+
+    @property
+    def points(self):
+        return tuple(self.__points)
 
     def __repr__(self):
         result = list()
@@ -40,64 +45,38 @@ class Track:
         elif all(
                 map(lambda x: isinstance(x, PointTrack), args)
         ):
-            return args
+            return list(args)
         else:
             raise ValueError("Неверные входные данные")
 
-    def add_back(self, pt):
+    def add_back(self, pt: PointTrack):
         """
         - добавление новой точки в конец маршрута(pt - объект класса PointTrack)
         """
+        self.__points.append(pt)
 
     def add_front(self, pt):
         """
         - добавление новой точки в начало маршрута(pt - объект класса PointTrack)
         """
+        self.__points = [pt, ] + self.__points
 
     def pop_back(self):
         """
         - удаление последней точки из маршрута
         """
+        self.__points.pop()
 
     def pop_front(self):
         """
         - удаление первой точки из маршрута
         """
+        self.__points.pop(0)
 
 
 if __name__ == "__main__":
-    # start_x, start_y = 1, 2
-    start_x, start_y = PointTrack(1, 2), PointTrack(3, 4)
-    tr = Track(start_x, start_y)
-    print(tr)
-
-    # @pytest.mark.parametrize('arguments', [
-    #     (15, -12.3),
-    #     (PointTrack(0.23, -54), PointTrack(-1, 101),),
-    #     (PointTrack(0.23, -54),),
-    # ])
-    # def test_track_class_structure(arguments):
-    #     assert hasattr(Track, "points")
-    #     assert type(getattr(Track, "points")) is property
-    #     assert hasattr(Track, 'add_back')
-    #     assert callable(getattr(Track, 'add_back'))
-    #     assert hasattr(Track, 'add_front')
-    #     assert callable(getattr(Track, 'add_front'))
-    #     assert hasattr(Track, 'pop_back')
-    #     assert callable(getattr(Track, 'pop_back'))
-    #     assert hasattr(Track, 'pop_front')
-    #     assert callable(getattr(Track, 'pop_front'))
-    #
-    #     tr = Track(*arguments)
-    #     assert hasattr(tr, "points")
-    #     assert type(getattr(tr, "points")) is tuple
-    #
-    #
-    # def test_point_track_class_structure():
-    #     pt = PointTrack(-12, 22.43)
-    #     assert hasattr(pt, 'x')
-    #     assert hasattr(pt, 'y')
-    #     with pytest.raises(TypeError) as ex:
-    #         pt_error = PointTrack(-11, [22, 4, 7])
-    #     assert 'координаты должны быть числами' in str(ex.value)
-    #     assert str(pt) == f"PointTrack: -12, 22.43"
+    tr = Track(PointTrack(0, 0), PointTrack(1.2, -0.5), PointTrack(2.4, -1.5))
+    tr.add_back(PointTrack(1.4, 0))
+    tr.pop_front()
+    for pt in tr.points:
+        print(pt)
