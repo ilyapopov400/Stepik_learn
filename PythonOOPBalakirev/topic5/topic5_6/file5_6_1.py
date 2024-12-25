4649e146c1194f4bdc91a3f9df99fd12d0a5c237
ilyapopov400 <147233832+ilyapopov400@users.noreply.github.com>
2024-12-25 15:43:59 +0300

    столкновение двух вертикальных кораблей

--- PythonOOPBalakirev/topic5/topic5_6/file5_6_1.py
+++ PythonOOPBalakirev/topic5/topic5_6/file5_6_1.py
@@ -103,17 +103,25 @@ class Ship:
             """
             - self вертикальный
             """
-            self_x_end, self_y_end = self_x_start, self_y_start + self.length
+            self_x_end, self_y_end = self_x_start, self_y_start + self.length - 1
             if ship.tp == 1:  # TODO
                 """
                 - self вертикальный
                 - other горизонтальный
                 """
-            else:  # TODO
+            else:
                 """
                 - self вертикальный
                 - other вертикальный
                 """
+                other_x_end, other_y_end = other_x_start, other_y_start + ship.length - 1
+                if abs(self_x_start - other_x_start) <= 1:
+                    if self_y_start - other_y_end >= 2:
+                        return False
+                    if other_y_start - self_y_end >= 2:
+                        return False
+                    return True
+
         return False
 
     def is_out_pole(self, size: int = 10) -> bool:
@@ -176,10 +184,14 @@ class GamePole:
 
 if __name__ == "__main__":
     a = Ship(length=4, x=2, y=2, tp=1)
-    b = Ship(length=3, x=7, y=3, tp=1)
+    b = Ship(length=3, x=9, y=3, tp=1)
 
     assert a.is_out_pole(size=10) is False, "ошибка выхода за поле игры"
-    assert b.is_out_pole(size=10) is False, "ошибка выхода за поле игры"
+    assert b.is_out_pole(size=10) is True, "ошибка выхода за поле игры, корабль за полем игры"
+
+    """
+    - столкновение двух горизонтальный кораблей
+    """
 
     a = Ship(length=4, x=2, y=2, tp=1)
     b = Ship(length=3, x=2, y=4, tp=1)
@@ -200,3 +212,35 @@ if __name__ == "__main__":
     a = Ship(length=4, x=2, y=2, tp=1)
     b = Ship(length=2, x=6, y=2, tp=1)
     assert a.is_collide(b) is True, "ошибка проверки на столкновение кораблей, нет столкновения"
+
+    """
+    - столкновение двух вертикальных кораблей
+    """
+
+    a = Ship(length=4, x=2, y=5, tp=2)
+    b = Ship(length=3, x=2, y=1, tp=2)
+    assert a.is_collide(b) is False, "ошибка проверки на столкновение кораблей, нет столкновения"
+
+    a = Ship(length=4, x=2, y=5, tp=2)
+    b = Ship(length=3, x=4, y=6, tp=2)
+    assert a.is_collide(b) is False, "ошибка проверки на столкновение кораблей, нет столкновения"
+
+    a = Ship(length=4, x=2, y=5, tp=2)
+    b = Ship(length=3, x=3, y=2, tp=2)
+    assert a.is_collide(b) is True, "ошибка проверки на столкновение кораблей, есть столкновение"
+
+    a = Ship(length=4, x=2, y=5, tp=2)
+    b = Ship(length=3, x=3, y=3, tp=2)
+    assert a.is_collide(b) is True, "ошибка проверки на столкновение кораблей, есть столкновение"
+
+    a = Ship(length=4, x=2, y=5, tp=2)
+    b = Ship(length=3, x=3, y=5, tp=2)
+    assert a.is_collide(b) is True, "ошибка проверки на столкновение кораблей, есть столкновение"
+
+    a = Ship(length=4, x=2, y=5, tp=2)
+    b = Ship(length=2, x=3, y=9, tp=2)
+    assert a.is_collide(b) is True, "ошибка проверки на столкновение кораблей, есть столкновение"
+
+    a = Ship(length=4, x=2, y=5, tp=2)
+    b = Ship(length=1, x=3, y=10, tp=2)
+    assert a.is_collide(b) is False, "ошибка проверки на столкновение кораблей, нет столкновения"
