1ddebc856f0ac0e04601fe542cf368d8c0096be4
ilyapopov400 <147233832+ilyapopov400@users.noreply.github.com>
2025-01-15 17:39:58 +0300

    db.py

---
+++ DeleteAm/db.py
@@ -0,0 +1,79 @@
+import sqlite3
+
+
+class DataLogs:
+    def __init__(self):
+        self.con = sqlite3.connect('log.db')
+
+        # открываем базу
+        with self.con:
+            # получаем количество таблиц с нужным нам именем
+            data = self.con.execute("select count(*) from sqlite_master where type='table' and name='logs'")
+            for row in data:
+                # если таких таблиц нет
+                if row[0] == 0:
+                    # создаём таблицу для товаров
+                    with self.con:
+                        self.con.execute("""
+                            CREATE TABLE logs (
+                                data_time TEXT,
+                                cpu REAL,
+                                ram REAL
+                            );
+                        """)
+
+    def __call__(self, data_time: str, cpu: float, ram: float):
+        """
+        - записываем данные в таблицу
+        :param data_time:
+        :param cpu:
+        :param ram:
+        :return:
+        """
+        self.con = sqlite3.connect('log.db')
+
+        # открываем базу
+        with self.con:
+            # получаем количество таблиц с нужным нам именем
+            data = self.con.execute("select count(*) from sqlite_master where type='table' and name='logs'")
+            for row in data:
+                # если таких таблиц нет
+                if row[0] == 0:
+                    # создаём таблицу для товаров
+                    with self.con:
+                        self.con.execute("""
+                                    CREATE TABLE logs (
+                                        data_time TEXT,
+                                        cpu REAL,
+                                        ram REAL
+                                    );
+                                """)
+        # подготавливаем множественный запрос
+        sql = 'INSERT INTO logs (data_time, cpu, ram) values(?, ?, ?)'
+        # указываем данные для запроса
+        data = [
+            (data_time, cpu, ram),
+
+        ]
+
+        # добавляем с помощью множественного запроса все данные сразу
+        with self.con:
+            self.con.executemany(sql, data)
+
+    def __repr__(self):
+        result = list()
+        with self.con:
+            data = self.con.execute("SELECT * FROM logs")
+            for row in data:
+                result.append(str(row))
+        result = "\n".join(result)
+        return result
+
+    def __del__(self):
+        self.con.close()
+
+
+if __name__ == "__main__":
+    db = DataLogs()
+    # db("Hello", 1, 2)
+    print(db)
--- DeleteAm/main.py
+++ DeleteAm/main.py
@@ -1,27 +1,49 @@
+"""
+- установлен PyGUI
+"""
+import time
 import dearpygui.dearpygui as dpg
 
 from cpu_view import StatisticsCPU
+from db import DataLogs
 
 dpg.create_context()
 dpg.create_viewport(title='Тестовое задание для DeleteAm', width=600, height=300)
+dpg.setup_dearpygui()
 
 
-def button_callback():
-    with dpg.window(label="CPU"):
+def start_writing_logs():
+    """
+    - функция для записи логов
+    :return:
+    """
+    while True:
         data_cpu = StatisticsCPU()
-        dpg.add_text("CPU: {}".format(data_cpu.cpu))
-        dpg.add_text("RUM: {}".format(data_cpu.ram))
-        print("Hello World!!!")
-        print("CPU: {}, RAM: {}".format(data_cpu.cpu, data_cpu.ram))
-        print("----------------")
+        cpu, ram, date_time = data_cpu.cpu, data_cpu.ram, str(
+            time.strftime("%x %X")
+        )
+        print("Начата запись логов: {}, {}, {}".format(date_time, cpu, ram))
+        with DataLogs as db:  # TODO
+            db = DataLogs()
+            db(date_time, cpu, ram)
+            time.sleep(3)
 
 
 if __name__ == "__main__":
+    dpg.show_viewport()
+
     with dpg.window(label="Window of CPU"):
         dpg.add_text("System boot information")
-        dpg.add_button(label="Start", callback=button_callback)
 
-    dpg.setup_dearpygui()
-    dpg.show_viewport()
+        data_cpu = StatisticsCPU()
+        cpu, ram = data_cpu.cpu, data_cpu.ram
+        dpg.add_text("CPU: {}%".format(cpu))
+        dpg.add_text("RUM: {}%".format(ram))
+
+        print("CPU: {}, RAM: {}".format(data_cpu.cpu, data_cpu.ram))
+        print("----------------")
+
+        dpg.add_button(label="Start writing logs", callback=start_writing_logs)
+
     dpg.start_dearpygui()
     dpg.destroy_context()
