d9095ffa7320b693b36ccebe051027106452c163
ilyapopov400 <147233832+ilyapopov400@users.noreply.github.com>
2025-02-17 09:51:20 +0300

    finish dictionary

--- Django/project2/dictionary/urls.py
+++ Django/project2/dictionary/urls.py
@@ -3,8 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('/', views.Home.as_view(), name='home'),
     path('home/', views.Home.as_view(), name='home'),
+    path('/home', views.Home.as_view(), name='home'),
     path('add_word/', views.AddWord.as_view(), name='add_word'),
+    path('/add_word', views.AddWord.as_view(), name='add_word'),
     path('words_list/', views.ShowWords.as_view(), name='show_words'),
+    path('/words_list', views.ShowWords.as_view(), name='show_words'),
 
 ]
