8b633c544219a18b4b11dd9e38ca0b5db4b456a6
ilyapopov400 <147233832+ilyapopov400@users.noreply.github.com>
2025-02-19 12:59:19 +0300

    base.html + bootstrap

--- Django/project3/notes/views.py
+++ Django/project3/notes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+class Index(View):
+    template_name = "notes/index.html"
+
+    def get(self, request):
+        return render(
+            request=request, template_name=self.template_name
+        )
