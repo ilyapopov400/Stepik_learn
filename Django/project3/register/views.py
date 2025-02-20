5d0db076a466514acc009d59f32b8bd49ea7ac9f
ilyapopov400 <147233832+ilyapopov400@users.noreply.github.com>
2025-02-20 14:22:27 +0300

    reg.html

--- Django/project3/register/views.py
+++ Django/project3/register/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.http import HttpResponse
 
@@ -7,9 +7,20 @@ from django.http import HttpResponse
 
 class UserReg(View):
     template_name = "register/reg.html"
+    template_name_redirect = "notes/index.html"
 
     def get(self, request):
         return render(request=request, template_name=self.template_name)
 
     def post(self, request):
-        return HttpResponse("Вы прошли регистрацию")
+        result = request.POST
+        username = result["username"]
+        first_name = result["first_name"]
+        last_name = result["last_name"]
+        email = result["email"]
+        password1 = result["password1"]
+        password2 = result["password2"]
+
+        print(username, first_name, last_name, email, password1, password2)
+
+        return render(request=request, template_name=self.template_name_redirect)
