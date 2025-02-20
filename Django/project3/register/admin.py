d816d9ee11bb6ed8663370bf6614bf2b3c8d0cc3
ilyapopov400 <147233832+ilyapopov400@users.noreply.github.com>
2025-02-20 15:45:50 +0300

    созаны модели, миграции не проведены

--- Django/project3/register/admin.py
+++ Django/project3/register/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CustomUser, Notes
+
+admin.site.register(CustomUser)
+admin.site.register(Notes)
--- Django/project3/register/models.py
+++ Django/project3/register/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+
+    def __str__(self):
+        return self.username
+
+
+class Notes(models.Model):
+    note = models.TextField()
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
