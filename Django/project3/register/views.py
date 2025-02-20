666e2ace83c915a66a49538600904e0ff10e6a00
ilyapopov400 <147233832+ilyapopov400@users.noreply.github.com>
2025-02-20 10:01:43 +0300

    register.reg.html без наполнения

--- Django/project3/register/views.py
+++ Django/project3/register/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+class UserReg(View):
+    template_name = "register/reg.html"
+
+    def get(self, request):
+        return render(request=request, template_name=self.template_name)
+
+    def post(self, request):
+        return HttpResponse("Вы прошли регистрацию")
