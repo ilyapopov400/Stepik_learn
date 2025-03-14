f46c7e88da15acdc53c77d95e46da4316e5934df
ilyapopov400 <147233832+ilyapopov400@users.noreply.github.com>
2025-03-14 11:10:57 +0300

    index.html

--- Django/project4/forms_for_book/app/views.py
+++ Django/project4/forms_for_book/app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+
+class Index(View):
+    template_name = "app/index.html"
+
+    def get(self, request):
+        return render(request=request, template_name=self.template_name)
